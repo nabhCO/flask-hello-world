16bd0d764ea5d0cf3f41209788c1a1c3fcf3c735
nabhCO <nabh6611@colorado.edu>
2026-04-05 16:27:19 +0000

    Implemented db_drop route, added comments

--- app.py
+++ app.py
@@ -1,19 +1,30 @@
+'''
+Author: Forked from https://github.com/render-examples/flask-hello-world.git, edits by Nandini Mahesh Bhat
+CU ID: nabh6611
+GitHub User Name: nabhCO
+'''
+
 from flask import Flask
 import psycopg2
 app = Flask(__name__)
 
+#index route (landing page)
 @app.route('/')
 def hello_world():
     return 'Hello World from Nandini Bhat in 3308'
+    
+    
 
-
+#route for testing database connection
 @app.route('/db_test')
 def db_test():
     conn = psycopg2.connect("postgresql://hello_world_db_v0i1_user:Jv5R5vjnTCEJ6rxXoBluoI7u8JKDg1zx@dpg-d797f66uk2gs73e8c7ug-a/hello_world_db_v0i1")
     conn.close()
     return "Successfully connected to database"
+    
+    
 
-
+#adds table called "Basketball" to database if it does not already exist (you can navigate to /db_drop to drop table before creating)
 @app.route('/db_create')
 def db_create():
     conn = psycopg2.connect("postgresql://hello_world_db_v0i1_user:Jv5R5vjnTCEJ6rxXoBluoI7u8JKDg1zx@dpg-d797f66uk2gs73e8c7ug-a/hello_world_db_v0i1")
@@ -32,8 +43,10 @@ def db_create():
     conn.commit()
     conn.close()
     return "Successfully created Basketball table"
-    
 
+
+    
+#adds data to the Basketball table
 @app.route('/db_insert')
 def db_insert():
     conn = psycopg2.connect("postgresql://hello_world_db_v0i1_user:Jv5R5vjnTCEJ6rxXoBluoI7u8JKDg1zx@dpg-d797f66uk2gs73e8c7ug-a/hello_world_db_v0i1")
@@ -53,6 +66,8 @@ def db_insert():
     return "Successfully added data to Basketball table"
 
 
+
+#selects and displays each of the rows in the Basketball table in the form of an HTML table
 @app.route('/db_select')
 def db_select():
 
@@ -78,7 +93,25 @@ def db_select():
     table_str += "</table>"
 
     return table_str
+
+
         
-        
+#drops the Basketball database (can be recreated with db_create)        
+@app.route('/db_drop')
+def db_drop():
+    conn = psycopg2.connect("postgresql://hello_world_db_v0i1_user:Jv5R5vjnTCEJ6rxXoBluoI7u8JKDg1zx@dpg-d797f66uk2gs73e8c7ug-a/hello_world_db_v0i1")
+    cur = conn.cursor()
+
+    cur.execute('''
+    DROP TABLE Basketball;
+    ''')
+
+    conn.commit()
+    conn.close()
+    return "Successfully dropped Basketball table"
+
+    
+
+    
     
 
