e6e5847f2e94826e860ae6387b370e3d822d3b4d
nabhCO <nabh6611@colorado.edu>
2026-04-05 15:46:06 +0000

    Implemented db_create route

--- app.py
+++ app.py
@@ -12,3 +12,25 @@ def db_test():
     conn = psycopg2.connect("postgresql://hello_world_db_v0i1_user:Jv5R5vjnTCEJ6rxXoBluoI7u8JKDg1zx@dpg-d797f66uk2gs73e8c7ug-a/hello_world_db_v0i1")
     conn.close()
     return "Successfully connected to database"
+
+
+@app.route('/db_create')
+def db_create():
+    conn = psycopg2.connect("postgresql://hello_world_db_v0i1_user:Jv5R5vjnTCEJ6rxXoBluoI7u8JKDg1zx@dpg-d797f66uk2gs73e8c7ug-a/hello_world_db_v0i1")
+    cur = conn.cursor()
+
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS Basketball(
+        First varchar(255),
+        Last varchar(255),
+        City varchar(255),
+        Name varchar(255),
+        Number int
+        );
+    ''')
+
+    conn.commit()
+    conn.close()
+    return "Successfully created Basketball table"
+    
+    
